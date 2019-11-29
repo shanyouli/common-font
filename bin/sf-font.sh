4a1bc783fa13f739af6a374991810deb1e1f30b1
shanyouli <shanyouli6@gmail.com>
2019-11-29 20:52:32 +0800

    add SFMonoFonts

--- bin/sf-font.sh
+++ bin/sf-font.sh
@@ -17,17 +17,34 @@
 # popd || echo "bad"
 
 # SF Compact字体下载
-SFCompact_URL="https://developer.apple.com/fonts/downloads/SFCompact.zip"
-SFCompact_DIR=/tmp/SFPro
-[[ -d ${SFCompact_DIR} ]] || mkdir -p ${SFCompact_DIR}
-pushd ${SFCompact_DIR} && {
-    wget -c ${SFCompact_URL} -O ${SFCompact_DIR}/SFCompact.zip
-    unzip SFCompact.zip
-    bsdtar xvPf ./SFCompact/'San Francisco Compact.pkg'
-    bsdtar xvPf ./'San Francisco Compact.pkg/Payload'
-    cd ./Library && mv -vf Fonts SFCompact
-    tar -Jcvf SFCompact.tar.xz SFCompact/*
-    mv SFCompact.tar.xz "${HOME}/SFCompact-$(date +%F).tar.xz"
+# SFCompact_URL="https://developer.apple.com/fonts/downloads/SFCompact.zip"
+# SFCompact_DIR=/tmp/SFPro
+# [[ -d ${SFCompact_DIR} ]] || mkdir -p ${SFCompact_DIR}
+# pushd ${SFCompact_DIR} && {
+#     wget -c ${SFCompact_URL} -O ${SFCompact_DIR}/SFCompact.zip
+#     unzip SFCompact.zip
+#     bsdtar xvPf ./SFCompact/'San Francisco Compact.pkg'
+#     bsdtar xvPf ./'San Francisco Compact.pkg/Payload'
+#     cd ./Library && mv -vf Fonts SFCompact
+#     tar -Jcvf SFCompact.tar.xz SFCompact/*
+#     mv SFCompact.tar.xz "${HOME}/SFCompact-$(date +%F).tar.xz"
+# }
+# rm -rf ${SFCompact_DIR}
+# popd || echo "bad"
+
+# SF Mono字体下载
+SFMono="SFMonoFonts"
+SFMono_URL="https://developer.apple.com/design/downloads/SF-Mono.dmg"
+SFMono_DIR=/tmp/${SFMono}
+[[ -d ${SFMono_DIR} ]] || mkdir -p ${SFMono_DIR}
+pushd ${SFMono_DIR} && {
+    wget -c ${SFMono_URL}
+    7z x SF-Mono.dmg
+    bsdtar xvPf ./${SFMono}/'SF Mono Fonts.pkg'
+    bsdtar xvPf ./'SFMonoFonts.pkg/Payload'
+    cd ./Library && mv -vf Fonts $SFMono
+    tar -Jcvf ${SFMono}.tar.xz ${SFMono}/*
+    mv ${SFMono}.tar.xz "${HOME}/${SFMono}-$(date +%F).tar.xz"
 }
-rm -rf ${SFCompact_DIR}
+# rm -rf ${SFMono_DIR}
 popd || echo "bad"
